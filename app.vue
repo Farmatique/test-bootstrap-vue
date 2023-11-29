11e02d4ef112ac1fb71a5a1ce83b9114b96f66df
Yevhen <ymarynych@mirantis.com>
2023-11-29 15:49:49 +0200

    init

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <BButton v-b-toggle.collapse-1 variant="primary">Toggle Collapse</BButton>
+
+<BCollapse id="collapse-1">
+  <BCard>
+    <p class="card-text">Collapse contents Here</p>
+    <BButton v-b-toggle.collapse-1-inner size="sm">Toggle Inner Collapse</BButton>
+    <BCollapse id="collapse-1-inner" class="mt-2">
+      <BCard>Hello!</BCard>
+    </BCollapse>
+  </BCard>
+</BCollapse>
+    
+  </div>
+</template>
